a362e3da725b62a339c738330e8f3cf895bdbda3
Nikita Isaev <keller108@yandex.ru>
2022-10-09 22:24:31 +0300

    Реализация Navbar и переключения состояний авторизации и регистрации

--- client/src/pages/AuthPage/AuthPage.tsx
+++ client/src/pages/AuthPage/AuthPage.tsx
@@ -2,6 +2,16 @@ import './AuthPage.css';
 
 export function AuthPage() {
     return (
-        <div>AuthPage</div>
+        <div className="auth-page">
+            <div className="auth-page__auth-container">
+                <h2 className="auth-page__form-title">Еще не зарегистрированы?</h2>
+                <h3 className="auth-page__form-title">Регистрация</h3>
+                <form className="auth-page__form">
+                    {/* <label htmlFor="email" className="auth-page__input-label"></label> */}
+                    <input type="text" className="auth-page__input" name="email"/>
+                    <input type="text" className="auth-page__input" name="password"/>
+                </form>
+            </div>
+        </div>
     )
 }
--- client/src/shared/Navbar/Navbar.tsx
+++ client/src/shared/Navbar/Navbar.tsx
@@ -1,18 +1,37 @@
+import { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import { observer } from 'mobx-react-lite';
+import { Context } from '../..';
 import { SHOP_ROUTE } from '../utils/const';
 import './Navbar.css';
 
-export function Navbar() {
+export const Navbar = observer(() => {
+    const { user } = useContext(Context);
+
+    let actionContent: JSX.Element[] | [] = [];
+
+    if (!user.isAuth) {
+        actionContent = [
+            <button onClick={() => user.setIsAuth(true)} className="navbar__action-btn">Войти</button>
+        ];
+    }
+
+    if (user.isAuth) {
+        actionContent = [
+            <button className="navbar__action-btn">Админ панель</button>,
+            <button className="navbar__action-btn" onClick={() => user.setIsAuth(false)}>Выйти</button>,
+            <button className="navbar__action-btn navbar__action-btn_type_basket" aria-label='Корзина'/>
+        ];
+    }
+
     return (
         <header className="navbar">
             <div className="navbar__content">
                 <NavLink to={SHOP_ROUTE}><h2 className="navbar__label">DeviceShop</h2></NavLink>
                 <div className="navbar__action-container">
-                    <button className="navbar__action-btn">Залогиниться</button>
-                    <button className="navbar__action-btn">Выйти</button>
-                    <button className="navbar__action-btn navbar__action-btn_type_basket" aria-label='Корзина'/>
+                    { actionContent }
                 </div>
             </div>
         </header>
     )
-}
+});
