4efbf4843eea2b128cdb03377dace31729670a54
Nikita Isaev <keller108@yandex.ru>
2022-10-09 16:41:15 +0300

    Добавлены сущности User и Device, Реализованы основные роуты

--- client/src/app/AppRouter.tsx
+++ client/src/app/AppRouter.tsx
@@ -1,17 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { useContext } from 'react';
+import { Routes, Navigate, Route } from "react-router-dom";
+import { Context } from "..";
 import { authRoutes, AuthRouteType, publicRoutes } from "../routes";
+import { SHOP_ROUTE } from '../shared/utils/const';
 
 export function AppRouter() {
-    const isAuth = false;
+    const { user } = useContext(Context);
 
     return (
         <Routes>
-            {isAuth && authRoutes.map(({ path, Component }: AuthRouteType) => 
+            {user.isAuth && authRoutes.map(({ path, Component }: AuthRouteType) => 
                 <Route key={path} path={path} element={Component()} />
             )}
             {publicRoutes.map(({ path, Component }: AuthRouteType) => 
                 <Route key={path} path={path} element={Component()} />
             )}
+            <Route path="/*" element={<Navigate replace={true} to={SHOP_ROUTE} />} />
         </Routes>
     )
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,14 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './app/App';
 import reportWebVitals from './reportWebVitals';
+import UserStore from './entities/User/store/UserStore';
+import DeviceStore from './entities/Device/store/DeviceStore';
+
+export const Context = React.createContext<any>(null);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      <Context.Provider value={{
+        user: new UserStore(),
+        device: new DeviceStore()
+      }}>
+        <App />
+      </Context.Provider>
+    </React.StrictMode>
 );
 
 reportWebVitals();
