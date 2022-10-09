eee3b201d85653baf2890f2fea60294da3b1e56c
Nikita Isaev <keller108@yandex.ru>
2022-10-09 11:22:58 +0300

    Добавлены функции контроллеров у сущностей

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,6 +1,27 @@
-class DeviceController {
-    async create(req, res) {
+const uuid = require('uuid');
+const path = require('path');
+const { Device } = require('../models/models');
+const ApiError = require('../error/ApiError');
 
+class DeviceController {
+    async create(req, res, next) {
+        try {
+            const { name, price, brandId, typeId, info } = req.body;
+            const { img } = req.files;
+
+            // Создание локального имени с помощью пакета uuid функции v4 (генерация id) ¯\_(ツ)_/¯
+            let fileName = uuid.v4() + ".jpg";
+
+            // Указываем серверу эти файлы отдавать как статику спомощью модуля path
+            img.mv(path.resolve(__dirname, '..', 'static', fileName));
+
+            const device = await Device.create({ name, price, brandId, typeId, img: fileName });
+
+            return res.json(device);
+
+        } catch (error) {
+            next(ApiError.badRequest(error.message));
+        }
     }
 
     async getAll(req, res) {
