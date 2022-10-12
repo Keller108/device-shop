acbf05d1fea0ec6da49c041e40638835778c26cf
Nikita Isaev <keller108@yandex.ru>
2022-10-12 21:54:33 +0300

    Работа с бд

--- client/src/features/AuthForm/api/auth.ts
+++ client/src/features/AuthForm/api/auth.ts
@@ -2,7 +2,7 @@ const REGISTRATION_URL = 'http://localhost:5000/api/user/registration';
 
 const handleResponse = (res: Response) => res.ok ? res.json() : Promise.reject(`Ошибка ${res.status}`)
 
-export const register = (email: string, password: string, role: string = "USER") => {
+export const register = (email: string, password: string, role: string = "ADMIN") => {
   return fetch(`${REGISTRATION_URL}`, {
     method: 'POST',
     headers: {
